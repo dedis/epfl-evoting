64d90cf9b890b10f3bfbe3dcdf9243a9391f225f
Gaurav Narula <gnarula94@gmail.com>
2018-03-22 17:56:12 +0100

    add evotingfrontend

---
+++ evoting/frontend/src/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  tequila: {
+    hostname: 'tequila.epfl.ch'
+  },
+  ldap: {
+    hostname: 'ldap.epfl.ch'
+  },
+  masterKey: new Uint8Array([84, 185, 18, 159, 67, 167, 151, 153, 235, 4, 231, 254, 81, 207, 164, 41, 214, 224, 204, 141, 237, 223, 155, 156, 93, 236, 68, 101, 97, 244, 138, 89]),
+  breadcrumbs: [
+    { text: 'EPFL', href: 'https://epfl.ch' },
+    { text: 'EPFL Assembly', href: 'https://ae.epfl.ch' },
+    { text: 'Elections 2018', href: 'https://ae.epfl.ch/elections' }
+  ]
+}
