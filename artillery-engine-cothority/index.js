fb1e46ad9cb698261a03f0d64b7a3f1cd9ce9411
Gaurav Narula <gnarula94@gmail.com>
2018-04-04 15:18:43 +0200

    add support for before/after request hooks

--- artillery-engine-cothority/index.js
+++ artillery-engine-cothority/index.js
@@ -4,6 +4,7 @@ const cothority = require("@dedis/cothority");
 const net = cothority.net;
 const debug = require("debug")("artillery-engine-cothority");
 const fs = require("fs");
+const traverse = require("traverse");
 
 class CothorityPlugin {
   constructor(script, ee, helpers) {
@@ -12,10 +13,22 @@ class CothorityPlugin {
   }
 
   createScenario(scenarioSpec, ee) {
-    let tasks = scenarioSpec.flow.map(rs => this.step(rs, ee));
-    console.dir(scenarioSpec.flow);
+    this._ensurePropertyIsAList(scenarioSpec, "beforeRequest");
+    this._ensurePropertyIsAList(scenarioSpec, "afterResponse");
+    let tasks = scenarioSpec.flow.map(rs => {
+      return this.step(rs, ee, {
+        beforeRequest: scenarioSpec.beforeRequest,
+        afterResponse: scenarioSpec.afterResponse
+      });
+    });
     return this.compile(tasks, scenarioSpec.flow, ee);
   }
+
+  _ensurePropertyIsAList(obj, prop) {
+    obj[prop] = [].concat(typeof obj[prop] === "undefined" ? [] : obj[prop]);
+    return obj;
+  }
+
   _capture(params, response, context, done) {
     let captures = {};
 
@@ -40,7 +53,61 @@ class CothorityPlugin {
       }
     );
   }
-  step(rs, ee) {
+
+  _template(o, context) {
+    let result;
+    const that = this;
+    if (typeof o === "object") {
+      result = traverse(o).map(function(x) {
+        if (typeof x === "string") {
+          this.update(that._template(x, context));
+        } else {
+          return x;
+        }
+      });
+    } else {
+      if (!/{{/.test(o)) {
+        return context.vars[o] || o;
+      }
+      const funcCallRegex = /{{\s*(\$[A-Za-z0-9_]+\s*\(\s*.*\s*\))\s*}}/;
+      let match = o.match(funcCallRegex);
+      if (match) {
+        // This looks like it could be a function call:
+        const syntax = esprima.parse(match[1]);
+        // TODO: Use a proper schema for what we expect here
+        if (
+          syntax.body &&
+          syntax.body.length === 1 &&
+          syntax.body[0].type === "ExpressionStatement"
+        ) {
+          let funcName = syntax.body[0].expression.callee.name;
+          let args = L.map(syntax.body[0].expression.arguments, function(arg) {
+            return arg.value;
+          });
+          if (funcName in context.funcs) {
+            return that._template(
+              o.replace(
+                funcCallRegex,
+                context.funcs[funcName].apply(null, args)
+              ),
+              context
+            );
+          }
+        }
+      } else {
+        if (!o.match(/{{/)) {
+          return context.vars[o] || o;
+        }
+
+        result = renderVariables(o, context.vars);
+      }
+    }
+    return result;
+  }
+
+  step(rs, ee, opts) {
+    opts = opts || {};
+    const { config } = this;
     if (rs.log) {
       return (context, callback) => {
         console.log(this.helpers.template(rs.log, context));
@@ -65,42 +132,90 @@ class CothorityPlugin {
 
     if (rs.send) {
       return (context, callback) => {
-        let { data, request, response } = rs.send;
-        data = this.helpers.template(data, context);
-        ee.emit("request");
-        const startedAt = process.hrtime();
+        let { data, request, response, beforeRequest, afterResponse } = rs.send;
+        const that = this;
 
-        debug(
-          `rosterSocket send: rosterSocket.send(${request}, ${response}, %o)`,
-          data
+        let functionNames = _.concat(
+          opts.beforeRequest || [],
+          beforeRequest || []
         );
-
-        context.ws
-          .send(request, response, data)
-          .then(reply => {
-            const endedAt = process.hrtime(startedAt);
-            const delta = endedAt[0] * 1e9 + endedAt[1];
-            ee.emit("response", delta, 0, context._uid);
-            this._capture(rs.send, reply, context, (err, captures) => {
+        async.eachSeries(
+          functionNames,
+          (functionName, next) => {
+            let processFunc = config.processor[functionName];
+            processFunc(context, ee, err => {
               if (err) {
-                // TODO: run onError hooks
-                ee.emit("error", err);
-                callback(err, context);
+                return next(err);
               }
-              debug("captures: %o", captures);
-
-              _.each(captures, (v, k) => {
-                context.vars[k] = v;
-              });
-              context._successCount++;
-              return callback(null, context);
+              return next(null);
             });
-          })
-          .catch(err => {
-            debug(err);
-            ee.emit("error", err);
-            callback(err, context);
-          });
+          },
+          err => {
+            if (err) {
+              console.log(err);
+              ee.emit("error", err);
+              return callback(err, context);
+            }
+            data = that._template(data, context);
+            debug("data: %o", data);
+            ee.emit("request");
+            const startedAt = process.hrtime();
+
+            debug(
+              `rosterSocket send: rosterSocket.send(${request}, ${response}, %o)`,
+              data
+            );
+
+            context.ws
+              .send(request, response, data)
+              .then(reply => {
+                const endedAt = process.hrtime(startedAt);
+                const delta = endedAt[0] * 1e9 + endedAt[1];
+                ee.emit("response", delta, 0, context._uid);
+                this._capture(rs.send, reply, context, (err, captures) => {
+                  if (err) {
+                    // TODO: run onError hooks
+                    ee.emit("error", err);
+                    return callback(err, context);
+                  }
+                  debug("captures: %o", captures);
+
+                  _.each(captures, (v, k) => {
+                    context.vars[k] = v;
+                  });
+                  const afterResponseFns = _.concat(
+                    opts.afterResponse || [],
+                    afterResponse || []
+                  );
+                  async.eachSeries(
+                    afterResponseFns,
+                    (fnName, next) => {
+                      let procFn = config.processor[fnName];
+                      procFn(reply, context, ee, err => {
+                        if (err) {
+                          return next(err);
+                        }
+                        return next(null);
+                      });
+                    },
+                    err => {
+                      if (err) {
+                        console.error(err);
+                        ee.emit("error", err);
+                        return callback(err, context);
+                      }
+                      return callback(null, context);
+                    }
+                  );
+                });
+              })
+              .catch(err => {
+                debug(err);
+                ee.emit("error", err);
+                return callback(err, context);
+              });
+          }
+        );
       };
     }
   }
@@ -126,6 +241,8 @@ class CothorityPlugin {
             cothority.Roster.fromTOML(roster),
             service
           );
+          initialContext.maxCount = this.config.maxCount || 1;
+          initialContext.voterCount = this.config.voterCount || 7;
           ee.emit("started");
           return next(null, initialContext);
         });
