0f6fcf003d57bd787f67a9bab2b286b7d9eab723
Gaurav Narula <gnarula94@gmail.com>
2018-05-03 01:17:30 +0200

    frontend: remove VueMaterial

--- evoting/frontend/src/main.js
+++ evoting/frontend/src/main.js
@@ -5,12 +5,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
-import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.min.css'
 import 'vuetify/dist/vuetify.min.css'
 import { store, i18n } from './store'
 
-Vue.use(VueMaterial)
 Vue.use(Vuetify)
 
 Vue.config.productionTip = false
