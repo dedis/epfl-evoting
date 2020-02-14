f9c89215bb1ae9a7d09f8023a27f0ee2db6377f7
Jeff R. Allen <jeff.allen@epfl.ch>
2020-02-14 12:17:12 +0100

    More updates to solve security probs, file upload button rendering prob.

--- evoting/frontend/src/components/UploadButton.vue
+++ evoting/frontend/src/components/UploadButton.vue
@@ -1,15 +1,7 @@
 <!-- From https://github.com/jannhama/vuetify-upload-btn/blob/master/UploadButton.vue -->
 <template>
-  <div class="upload-input input-group">
-    <div class="input-group__input">
-      <i aria-hidden="true" class="icon material-icons input-group__prepend-icon">{{ prependIcon }}</i>
-      <div class="btn btn-primary jbtn-file"> {{ title }}
-        <input type="file" v-on:change="fileSelected">
-      </div>
-      <div class="upload-info flex">{{ fileName || 'No file selected' }}</div>
-      <div class="sciper-info" v-if="$store.state.scipersReadFromFile !== 0">Read {{ $store.state.scipersReadFromFile }} scipers</div>
-    </div>
-  </div>
+  <div class="btn btn-primary jbtn-file"> {{ title }}<input
+    type="file" v-on:change="fileSelected"></div>
 </template>
 
 <script>
@@ -17,22 +9,12 @@
     name: 'upload-button',
     props: {
       selectedCallback: Function,
-      title: String,
-      prependIcon: String
-    },
-    data () {
-      return {
-        fileName: ''
-      }
-    },
-    created () {
-      this.$store.state.scipersReadFromFile = 0
+      title: String
     },
     methods: {
       fileSelected (e) {
         if (this.selectedCallback) {
           if (e.target.files[0]) {
-            this.fileName = e.target.files[0].name
             this.selectedCallback(e.target.files[0])
           } else {
             this.selectedCallback(null)
@@ -44,41 +26,4 @@
 </script>
 
 <style scoped>
-  .jbtn-file {
-    cursor: pointer;
-    position: relative;
-    overflow: hidden;
-  }
-
-  .jbtn-file input[type=file] {
-    position: absolute;
-    top: 0;
-    right: 0;
-    min-width: 100%;
-    min-height: 100%;
-    text-align: right;
-    filter: alpha(opacity=0);
-    opacity: 0;
-    outline: none;
-    cursor: inherit;
-    display: block;
-  }
-
-  .upload-info {
-    display: flex;
-    justify-content: left;
-    align-items: center;
-  }
-
-  .sciper-info {
-    display: flex;
-    justify-content: right;
-    align-items: center;
-    text-align: right;
-  }
-
-  .upload-input {
-    margin-bottom: 10px;
-  }
 </style>
-
